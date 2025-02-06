2d60537d5a59271a7dad1218ff65d2aef595afc0
Mainak <mainakhalder99@gmail.com>
2025-02-06 22:14:16 +0530

    chore: added vercel deployment and cors configuration

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.send("Server is working");
 });
